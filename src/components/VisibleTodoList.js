771c6737fe0bc5ab8510995d00d4c130149207d7
Eduardo Mauricio Mueller Nicastro <edu.nicastro@gmail.com>
2018-03-04 04:05:15 -0300

    Normalizing the state shape

--- src/components/VisibleTodoList.js
+++ src/components/VisibleTodoList.js
@@ -1,30 +1,22 @@
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
 import { toggleTodo } from '../actions';
+import { getVisibleTodos } from '../reducers';
 import TodoList from './TodoList';
 
-const getVisibleTodos = (todos, filter) => {
-	switch (filter) {
-		case 'all':
-			return todos;
-		case 'completed':
-			return todos.filter(t => t.completed);
-		case 'active':
-			return todos.filter(t => !t.completed);
-		default:
-			throw new Error(`Unknown filter: ${filter}.`);
-	}
-};
-
-const mapStateToProps = (state, ownProps) => ({
-	todos: getVisibleTodos(state.todos, ownProps.filter)
+const mapStateToProps = (state, { params }) => ({
+	todos: getVisibleTodos(state, params.filter || 'all')
 });
 
-const mapDispatchToProps = dispatch => ({
-	onTodoClick(id) {
-		dispatch(toggleTodo(id));
-	}
-});
+//
+// const mapDispatchToProps = dispatch => ({
+// 	onTodoClick(id) {
+// 		dispatch(toggleTodo(id));
+// 	}
+// });
 
-const VisibleTodoList = connect(mapStateToProps, mapDispatchToProps)(TodoList);
+const VisibleTodoList = withRouter(
+	connect(mapStateToProps, { onTodoClick: toggleTodo })(TodoList)
+);
 
 export default VisibleTodoList;
